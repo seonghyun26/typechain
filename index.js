e61c96a377993d9585f62fd09d324468c32a54d6
eddy <eddy_2000@naver.com>
2021-06-25 12:33:15 +0900

    Really Basic Typescript

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'eddy', age = 24, gender = 'male';
+const sayHI = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+/*
+    sayHI(name, age);
+    On Javascript, hello eddy, you are 24, you are a undefined.
+*/
+sayHI(name, age);
+sayHI(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello");
+const name = 'eddy',
+    age = 24,
+    gender = 'male';
+
+// ? : means parameter is optional
+    const sayHI = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+/*
+    sayHI(name, age);
+    On Javascript, hello eddy, you are 24, you are a undefined.
+*/
+
+sayHI(name, age, gender);
+
+export {};
